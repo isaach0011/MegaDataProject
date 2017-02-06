9d262802156c38503af7cf249cb49c4f3e82b2ba
isaach0011 <ihil17523@740s-pl-121.local>
2017-02-06 13:20:39 -0700

    Created the IntNode header file.

---
+++ MegaDataProject/Model/IntNode.cpp
@@ -0,0 +1,9 @@
+//
+//  IntNode.cpp
+//  MegaDataProject
+//
+//  Created by Hill, Isaac on 2/6/17.
+//  Copyright © 2017 Hill, Isaac. All rights reserved.
+//
+
+#include "IntNode.hpp"
---
+++ MegaDataProject/Model/IntNode.hpp
@@ -0,0 +1,34 @@
+//
+//  IntNode.hpp
+//  MegaDataProject
+//
+//  Created by Hill, Isaac on 2/6/17.
+//  Copyright © 2017 Hill, Isaac. All rights reserved.
+//
+
+#ifndef IntNode_hpp
+#define IntNode_hpp
+
+class IntNode
+{
+private:
+    int nodeData;
+    IntNode * nodePointer;
+public:
+    //Constructor
+    IntNode();
+    IntNode(int value);
+    /*
+     Creates an IntNode with a specified value and next node pointer.
+     */
+    IntNode(int value, IntNode * nextNode);
+    
+    //Methods
+    int getNodeData();
+    IntNode * getNodePointer();
+    
+    void setNodeData(int value);
+    void setNodePointer(IntNode * next);
+};
+
+#endif /* IntNode_hpp */
