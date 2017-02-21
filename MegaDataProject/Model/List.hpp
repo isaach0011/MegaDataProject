065c270fb9e54b004eb3bb909ae529f3cfd3acc1
isaach0011 <ihil17523@740s-pl-121.local>
2017-02-21 13:38:13 -0700

    Started List.hpp and created header.

---
+++ MegaDataProject/Model/List.hpp
@@ -0,0 +1,51 @@
+//
+//  List.hpp
+//  MegaDataProject
+//
+//  Created by Hill, Isaac on 2/21/17.
+//  Copyright © 2017 Hill, Isaac. All rights reserved.
+//
+
+#ifndef List_hpp
+#define List_hpp
+
+#include "Node.hpp"
+#include <assert.h>
+#include <iostream>
+
+using namespace std;
+
+template <class Type>
+class List
+{
+private:
+    int size;
+    Node<Type> * front;
+public:
+    List();
+    List<Type>(const List<Type> & source);
+    ~List<Type>();
+  
+    void addAtIndex(int index, Type value);
+    void add(Type value);
+    Type remove(int index);
+    Type setAtIndex(int index, Type data);
+    Type getFromIndex(int index);
+    bool contains(Type data);
+    int getSize() const;
+    Node<Type> * getFront() const;
+};
+
+
+template <class Type>
+List<Type> :: List()
+{
+    
+}
+
+template <class Type>
+List<Type :: List(const List<Type> & source)
+{
+    
+}
+#endif /* List_hpp */
