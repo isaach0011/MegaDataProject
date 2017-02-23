d467e428e797a2ab5e92d5a014492d01b43c1c90
isaach0011 <ihil17523@740s-pl-121.local>
2017-02-23 13:53:29 -0700

    Added add front add end and add at index methods to list

--- MegaDataProject/Model/List.hpp
+++ MegaDataProject/Model/List.hpp
@@ -21,20 +21,25 @@ class List
 private:
     int size;
     Node<Type> * front;
+    Node<Type> * end;
+    
 public:
     List();
     List<Type>(const List<Type> & source);
     ~List<Type>();
-  
+    List<Type>& operator =(const List<Type> & replacingList);
+    
+    int getSize() const;
+    Node<Type> * getFront() const;
+    Node<Type> * getEnd() const;
+    
     void addAtIndex(int index, Type value);
     void addFront(Type value);
     void addEnd(Type value);
     Type remove(int index);
     Type setAtIndex(int index, Type data);
     Type getFromIndex(int index);
-    bool contains(Type data);
-    int getSize() const;
-    Node<Type> * getFront() const;
+    bool contains(Type value);
 };
 
 
@@ -57,9 +62,43 @@ List<Type> :: ~List()
 }
 
 template <class Type>
-void List<Type> :: addAtIndex(int index, Type value)
+List<Type> :: List<Type>& operator =(const List<Type> & replacingList)
 {
     
+}
+
+template <class Type>
+List<Type>
+template <class Type>
+void List<Type> :: addAtIndex(int index, Type value)
+{
+    assert(index >= 0 && index <= size);
+    
+    if(index == 0)
+    {
+        addFront(value);
+    }
+    else if(index == size)
+    {
+        addEnd(value);
+    }
+    else
+    {
+        Node<Type> * insertedNode = new Node<Type>(value);
+        Node<Type> * current = front;
+        Node<Type> * pervious = nullptr;
+        
+        for(int position = 0; position < index; position++)
+        {
+            previous = current;
+            current = current->getNodePointer();
+        }
+        
+        previous->setNodePointer(insertedNode);
+        insertedNode->setNodePointer(current);
+        
+        size++;
+    }
 }
 
 template <class Type>
@@ -103,13 +142,82 @@ void List<Type> :: addEnd(Type data)
 template <class Type>
 Type List<Type> :: remove(int index)
 {
+    assert(index >= 0 && index < size);
+    Type removed;
     
+    Node<Type> * current = front;
+    Node<Type> * previous = nullptr;
+    Node<Type> * toBeRemoved = nullptr;
+    
+    if(index == 0)
+    {
+        toBeRemoved = front;
+        this->front = front->getNodePointer();
+    }
+    else if(idnex == size - 1)
+    {
+        for(int spot = 0; spot < index; spot++)
+        {
+            pervious = current;
+            current = current->getNodePointer();
+        }
+        
+        toBeRemoved = current;
+        previous->setNodePointer(nullptr);
+        this->end = previous;
+    }
+    else
+    {
+        for(int spot = 0; spot < index; spot++)
+        {
+            previous = current;
+            current = current->getNodePointer();
+        }
+        
+        toBeRemoved = current;
+        current = toBeRemoved->getNodePointer();
+        previous->setNodePointer(current);
+        
+    }
+    removed = toBeRemoved->getNodeData();
+    
+    delete toBeRemoved;
+    
+    size--;
+    return removed;
 }
 
 template <class Type>
 Type List<Type> :: setAtIndex(int index, Type data)
 {
+    assert(index >= 0 && index < size)
+    Type removedData;
     
+    if(index == 0)
+    {
+        addFront(value);
+    }
+    else if(index == size)
+    {
+        addEnd(value);
+    }
+    else
+    {
+        Node<Type> * insertedNode = new Node<Type>(value);
+        Node<Type> * current = front;
+        Node<Type> * pervious = nullptr;
+        
+        for(int position = 0; position < index; position++)
+        {
+            previous = current;
+            current = current->getNodePointer();
+        }
+        
+        previous->setNodePointer(insertedNode);
+        insertedNode->setNodePointer(current);
+        
+        size++;
+    }
 }
 
 template <class Type>
