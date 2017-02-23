2d6c23b17e6b4a48634f40d6752119fd711d4d1a
isaach0011 <ihil17523@740s-pl-121.local>
2017-02-23 12:46:20 -0700

    Added methods.

--- MegaDataProject/Model/List.hpp
+++ MegaDataProject/Model/List.hpp
@@ -45,16 +45,95 @@ List<Type> :: List()
 }
 
 template <class Type>
-List<Type :: List(const List<Type> & source)
+List<Type> :: List(const List<Type> & source)
 {
     
 }
 
 template <class Type>
-List<Type :: ~List()
+List<Type> :: ~List()
 {
     
 }
 
+template <class Type>
+void List<Type> :: addAtIndex(int index, Type value)
+{
+    
+}
+
+template <class Type>
+void List<Type> :: addFront(Type value)
+{
+    if(size == 0)
+    {
+        Node<Type> * first = new Node<Type>(value);
+        this->front = first;
+        this->end = first;
+    }
+    else
+    {
+        Node<Type> * newFirst = new Node<Type>();
+        newFirst->setNodeData(value);
+        newFirst->NodePointer(front);
+        
+        this->front = newFirst;
+    }
+    
+    size++;
+}
+
+template <class Type>
+void List<Type> :: addEnd(Type data)
+{
+    Node<Type> * added = new Node<Type>(data);
+    if(size == 0)
+    {
+        this->front = added;
+        this->end = added;
+    }
+    else
+    {
+        end->setNodePointer(added);
+        this->end = added;
+    }
+    size++;
+}
+
+template <class Type>
+Type List<Type> :: remove(int index)
+{
+    
+}
+
+template <class Type>
+Type List<Type> :: setAtIndex(int index, Type data)
+{
+    
+}
+
+template <class Type>
+Type List<Type> ::getFromIndex(int index)
+{
+    
+}
+
+template <class Type>
+bool List<Type> :: conatins(Type data)
+{
+    
+}
+
+template <class Type>
+int List<Type> :: getSize() const
+{
+    
+}
+
+template <class Type>
+Node<Type> * List<Type> :: getFront() const
+{
+    
+}
 
 #endif /* List_hpp */
