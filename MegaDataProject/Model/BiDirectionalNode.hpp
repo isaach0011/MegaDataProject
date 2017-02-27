ef9af44e52ef34dcf717b2f7a2fec2a69f0082b6
isaach0011 <ihil17523@740s-pl-121.local>
2017-02-27 13:53:19 -0700

    Added BiDirectionalNode file to project and started framework.

---
+++ MegaDataProject/Model/BiDirectionalNode.hpp
@@ -0,0 +1,37 @@
+//
+//  BiDirectionalNode.hpp
+//  MegaDataProject
+//
+//  Created by Hill, Isaac on 2/27/17.
+//  Copyright © 2017 Hill, Isaac. All rights reserved.
+//
+
+#ifndef BiDirectionalNode_h
+#define BiDirectionalNode_h
+
+template <class Type>
+class BiDirectionalNode : public Node<Type>
+{
+private:
+    BiDirectionalNode<Type> * previous;
+    BiDirecitonalNode<Type> * next;
+    Type data;
+public:
+    BiDirectionalNode();
+    BiDirectionalNode(Type data);
+    BiDirectionalNode(Type data, BiDirectionalNode<Type> * previous, BiDirectionalNode<Type> * next);
+    
+}
+
+template <class Type>
+BiDirectionalNode<Type> :: BiDirectionalNode() : Node()
+{
+    
+}
+
+template <class Type>
+BiDirectionalNode<Type> :: BiDirectionalNode(Type data) : Node<Type>(data)
+{
+    
+}
+#endif /* BiDirectionalNode_h */
