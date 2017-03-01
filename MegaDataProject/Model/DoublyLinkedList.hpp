0127824d92cacf21f8ebc9a765feaf88bf966156
isaach0011 <ihil17523@740s-pl-121.local>
2017-03-01 12:52:17 -0700

    Added DoublyLinkedList.hpp and added it to UML

---
+++ MegaDataProject/Model/DoublyLinkedList.hpp
@@ -0,0 +1,20 @@
+//
+//  DoublyLinkedList.hpp
+//  MegaDataProject
+//
+//  Created by Hill, Isaac on 3/1/17.
+//  Copyright © 2017 Hill, Isaac. All rights reserved.
+//
+
+#ifndef DoublyLinkedList_h
+#define DoublyLinkedList_h
+
+#include "BiDirecitonalNode.hpp"
+
+template <class Type>
+class DoublyLinkedList
+{
+    
+};
+
+#endif /* DoublyLinkedList_h */
