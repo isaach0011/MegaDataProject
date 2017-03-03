0dc872bf0e538a943962ea2fbe0f5009d8e147f1
isaach0011 <ihil17523@740s-pl-123.local>
2017-03-03 13:05:08 -0700

    Finished uncompleted parts of UML and finished methods in DoublyLinkedList

--- MegaDataProject/Model/DoublyLinkedList.hpp
+++ MegaDataProject/Model/DoublyLinkedList.hpp
@@ -21,13 +21,16 @@ private:
 public:
     DoublyLinkedList();
     virtual ~DoublyLinkedList() = 0;
+    virtual void add(Type data) = 0;
+    virtual Type remove(int index) = 0;
     
     int getSize() const;
     BiDirectionalNode<Type> * getFront() const;
     BiDirectionalNode<Type> * getEnd() const;
     
-    virtual void add(Type data) = 0;
-    virtual Type remove(int index) = 0;
+    void setFront(BiDirectionalNode<Type> * front);
+    void setSize(int updated);
+    void setEnd(BiDirectionalNode<Type> * end);
 };
 
 template <class Type>
@@ -36,6 +39,12 @@ DoublyLinkedList<Type> :: DoublyLinkedList()
     size = 0;
     front = nullptr;
     end = nullptr;
+}
+
+template <class Type>
+DoublyLinkedList<Type> :: ~DoublyLinkedList()
+{
+    //Implemented only to avoid errors.
 }
 
 template <class Type>
@@ -54,6 +63,24 @@ template <class Type>
 BiDirectionalNode<Type> * DoublyLinkedList<Type> :: getEnd() const
 {
     return end;
+}
+
+template <class Type>
+void DoublyLinkedList<Type> :: setSize(int size)
+{
+    this->size = size;
+}
+
+template <class Type>
+void DoublyLinkedLIst<Type> :: setFront(BiDirectionalNode<Type> * front)
+{
+    this->front = front;
+}
+
+template <class Type>
+void DoublyLinkedList<Type> :: setEnd(BiDirectionalNode<Type> * end)
+{
+    this->end = end;
 }
 
 #endif /* DoublyLinkedList_hpp */
