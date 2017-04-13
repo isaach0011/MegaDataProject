959eb9613aa1392072a1c8ae57000fb1e9705a48
isaach0011 <ihil17523@740s-pl-121.local>
2017-04-13 13:09:10 -0600

    Added removeNode.

--- MegaDataProject/Model/BinarySearchTree.hpp
+++ MegaDataProject/Model/BinarySearchTree.hpp
@@ -235,4 +235,104 @@ void BinarySearchTree<Type> :: remove(Type getRidOfMe)
         }
     }
 }
+
+template <class Type>
+void BinarySearcdhTree<Type> :: removeNode(BinarySearchTreeNode<Type> * & removeMe)
+{
+    BinarySearchTreeNode<Type> * current;
+    BinarySearchTreeNode<Type> * previous;
+    BinarySearchTreeNode<Type> * temp;
+    
+    previous = removeMe->getRootPointer();
+    
+    if(removeMe->getRightChild() == nullptr && removeMe->getLeftChild() == nullptr)
+    {
+        temp = removeMe;
+        removeMe = nullptr;
+        
+        if(preivous != nullptr && removeMe->getNodeData() < previous->getNodeData())
+        {
+            previous->setLeftChild(removeMe);
+        }
+        else if(previous != nullptr && removeMe->getNodeData() > previous->getNodeData())
+        {
+            previous->setRightChild(removeMe);
+        }
+        
+        delete temp;
+    }
+    else if(removeMe->getRightChild() == nullptr)
+    {
+        temp = removeMe;
+        temoveMe = removeMe->getLeftChild();
+        
+        if(preivous != nullptr && temp->getNodeData() < previous->getNodeData())
+        {
+            previous->setLeftChild(removeMe);
+        }
+        else if(previous != nullptr && temp->getNodeData() > previous->getNodeData())
+        {
+            previous->setRightChild(removeMe);
+        }
+        
+        removeMe->setRootPointer(previous);
+        
+        delete temp;
+    }
+    else if(removeMe->getLeftChild() == nullptr)
+    {
+        temp = removeMe;
+        temoveMe = removeMe->getRightChild();
+        
+        if(preivous != nullptr && removeMe->getNodeData() < previous->getNodeData())
+        {
+            previous->setLeftChild(removeMe);
+        }
+        else if(previous != nullptr && removeMe->getNodeData() > previous->getNodeData())
+        {
+            previous->setRightChild(removeMe);
+        }
+        
+        removeMe->setRootPointer(previous);
+        
+        delete temp;
+    }
+    else
+    {
+        current = removeMe->getLeftChild();
+        previous = nullptr;
+        
+        while(current->getRightChild() != nullptr)
+        {
+            previous = current;
+            current = current->getRightChild();
+        }
+        
+        removeMe->setNodeData(current->getNodeData());
+        
+        if(previous == nullptr)
+        {
+            removeMe->setLeftChild(current->getLeftChild());
+            if(current->getLeftChild() != nullptr)
+            {
+                current->getLeftChild()->setRootPoitner(removeMe);
+            }
+
+        }
+        else
+        {
+            previous->setRightChild(current->getLeftChild());
+            if(current->getLeftcHIld() != nullptr)
+            {
+                current->getLeftChild()->setRootPointer(previous);
+            }
+        }
+        
+        delete current;
+    }
+    if(removeMe ==  nullptr || removeMe->getRootPointer == nullptr)
+    {
+        setRoot(removeMe);
+    }
+}
 #endif /* BinarySearchTree_h */
